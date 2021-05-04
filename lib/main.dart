c5cce0ad5391297e4918b97f3fee63e9128913d7
jcarter <jcarter62@gmail.com>
2021-05-04 15:20:40 -0700

    finish up

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'dart:io';
-//import 'package:image/image.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,7 +14,16 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.teal,
           body: SafeArea(
               child: Column(
-            children: [buildCircleAvatar(), buildUserText()],
+                mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              buildCircleAvatar(),
+              buildUserText(),
+              buildPosition(),
+              buildHLine(),
+              buildPhoneInfo(),
+              buildSpacer(),
+              buildEmail()
+            ],
           ))),
     );
   }
@@ -24,7 +32,23 @@ class MyApp extends StatelessWidget {
     return Text(
       'Jim Carter',
       style: TextStyle(
-          color: Colors.white, fontFamily: 'Anton', fontSize: 24.0),
+          color: Colors.white,
+          fontFamily: 'Anton',
+          fontStyle: FontStyle.normal,
+          fontSize: 24.0),
+    );
+  }
+
+  Text buildPosition() {
+    return Text(
+      'Information Officer',
+      style: TextStyle(
+        color: Colors.white,
+        fontFamily: 'Anton',
+        fontStyle: FontStyle.normal,
+        fontSize: 16,
+        letterSpacing: 2.0,
+      ),
     );
   }
 
@@ -34,89 +58,56 @@ class MyApp extends StatelessWidget {
       backgroundImage: AssetImage('images/headshot.jpg'),
     );
   }
-}
 
-// class MyHomePage extends StatefulWidget {
-//   MyHomePage({Key key, this.title}) : super(key: key);
-//
-//   // This widget is the home page of your application. It is stateful, meaning
-//   // that it has a State object (defined below) that contains fields that affect
-//   // how it looks.
-//
-//   // This class is the configuration for the state. It holds the values (in this
-//   // case the title) provided by the parent (in this case the App widget) and
-//   // used by the build method of the State. Fields in a Widget subclass are
-//   // always marked "final".
-//
-//   final String title;
-//
-//   @override
-//   _MyHomePageState createState() => _MyHomePageState();
-// }
-//
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-//
-//   void _incrementCounter() {
-//     setState(() {
-//       // This call to setState tells the Flutter framework that something has
-//       // changed in this State, which causes it to rerun the build method below
-//       // so that the display can reflect the updated values. If we changed
-//       // _counter without calling setState(), then the build method would not be
-//       // called again, and so nothing would appear to happen.
-//       _counter++;
-//     });
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     // This method is rerun every time setState is called, for instance as done
-//     // by the _incrementCounter method above.
-//     //
-//     // The Flutter framework has been optimized to make rerunning build methods
-//     // fast, so that you can just rebuild anything that needs updating rather
-//     // than having to individually change instances of widgets.
-//     return Scaffold(
-//       appBar: AppBar(
-//         // Here we take the value from the MyHomePage object that was created by
-//         // the App.build method, and use it to set our appbar title.
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         // Center is a layout widget. It takes a single child and positions it
-//         // in the middle of the parent.
-//         child: Column(
-//           // Column is also a layout widget. It takes a list of children and
-//           // arranges them vertically. By default, it sizes itself to fit its
-//           // children horizontally, and tries to be as tall as its parent.
-//           //
-//           // Invoke "debug painting" (press "p" in the console, choose the
-//           // "Toggle Debug Paint" action from the Flutter Inspector in Android
-//           // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-//           // to see the wireframe for each widget.
-//           //
-//           // Column has various properties to control how it sizes itself and
-//           // how it positions its children. Here we use mainAxisAlignment to
-//           // center the children vertically; the main axis here is the vertical
-//           // axis because Columns are vertical (the cross axis would be
-//           // horizontal).
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headline4,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-//}
+  Card buildPhoneInfo() {
+    return Card(
+        margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 35.0),
+        child: ListTile(
+            leading: Icon(Icons.phone, color: Colors.teal),
+            title: Text(
+              '559-241-6243',
+              style: TextStyle(
+                  color: Colors.teal,
+                  fontFamily: 'Anton',
+                  fontSize: 16.0,
+                  letterSpacing: 2.0),
+            )));
+  }
+
+  Card buildEmail() {
+    return Card(
+        margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 35.0),
+        child: ListTile(
+            leading: Icon(Icons.email, color: Colors.teal),
+            title: Text(
+              'jcarter@wwd.ca.gov',
+              style: TextStyle(
+                  color: Colors.teal,
+                  fontFamily: 'Anton',
+                  fontSize: 16.0,
+                  letterSpacing: 2.0),
+            )));
+  }
+
+  Container buildSpacer() {
+    return Container(
+      child: SizedBox(
+        width: 5.0,
+        height: 5.0,
+      ),
+    );
+  }
+
+  Container buildHLine() {
+    return Container(
+      child: SizedBox(
+        height: 20.0,
+          width: 150,
+          child: Divider(
+            color: Colors.teal.shade100,
+          ),
+      )
+    );
+  }
+
+}
